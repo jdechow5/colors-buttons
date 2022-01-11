ba81db8d40b2293434c1182b339e1d14788e55bc
Will Greenberg <greenberg.will@gmail.com>
2022-01-11 10:57:19 -0800

    initial commit for background button pages
    Co-authored-by Daniel Lindsey <daniellindsey85254@gmail.com>
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("widthClass");
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("#dark").toggle();
+    $("#light").toggle();
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+    $("#dark").toggle();
+    $("#light").toggle();
+  });
+
+});
